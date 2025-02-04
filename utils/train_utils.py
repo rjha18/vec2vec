b31533f99f87119967d35eb6c7b67dba1d0322fa
rjha18 <rjha18@live.com>
2025-02-04 14:24:07 -0500

    Fixing vsp

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -42,22 +42,30 @@ def trans_loss_fn(ins, translations, logger, prefix=""):
     return (loss / len(ins))
 
 
-def vsp_loss_fn(ins, translations, logger):
-    assert ins.keys() == translations.keys()
+def vsp_loss_fn(ins, translations, logger) -> torch.Tensor:
     loss = None
-    # TODO: Abstract this to work properly with non-unit vectors.
-    for target_flag in ins.keys():
-        in_distances = 1 - (ins[target_flag] @ ins[target_flag].T)
-        for flag in translations[target_flag].keys():
-            out_distances = 1 - (translations[target_flag][flag] @ translations[target_flag][flag].T)
-            vsp_loss = (in_distances - out_distances).abs().mean()
-            logger.logkv(f"{flag}_{target_flag}_vsp", vsp_loss)
-            
-        if loss is None:
-            loss = vsp_loss
-        else:
-            loss += vsp_loss
-    return (loss / len(ins))
+    EPS = 1e-10
+    count = 0
+    for out_name in ins.keys():
+        if out_name not in translations: continue # skip for unidirectional version
+        B = ins[out_name]
+        B = B / (B.norm(dim=1, keepdim=True) + EPS)
+        in_sims = B @ B.T
+        for in_name in translations[out_name].keys():
+            if in_name == out_name: continue # don't learn the identity
+            A = translations[out_name][in_name]
+            A = A / (A.norm(dim=1, keepdim=True) + EPS)
+            out_sims = A @ A.T
+            vsp_loss = (in_sims - out_sims).abs().mean()
+            if logger is not None:
+                logger.logkv(f"{in_name}_{out_name}_vsp", vsp_loss)
+
+            if loss is None:
+                loss = vsp_loss
+            else:
+                loss += vsp_loss
+            count += 1
+    return loss / count
 
 
 def get_grad_norm(model: torch.nn.Module) -> torch.Tensor:
